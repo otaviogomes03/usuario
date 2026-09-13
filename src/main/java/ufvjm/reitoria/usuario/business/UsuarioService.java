66b3051b47c4670c4923febe3f9fa47ddff08f2a
Otávio Gomes <otaviogomes0303@gmail.com>
2026-09-13 12:38:02 -0300

    feat: add get, delete, and login methods

--- src/main/java/ufvjm/reitoria/usuario/business/UsuarioService.java
+++ src/main/java/ufvjm/reitoria/usuario/business/UsuarioService.java
@@ -1,10 +1,13 @@
 package ufvjm.reitoria.usuario.business;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import ufvjm.reitoria.usuario.business.dto.UsuarioDTO;
 import ufvjm.reitoria.usuario.converter.UsuarioConverter;
 import ufvjm.reitoria.usuario.infrastructure.entity.Usuario;
+import ufvjm.reitoria.usuario.infrastructure.exceptions.ConflictException;
+import ufvjm.reitoria.usuario.infrastructure.exceptions.ResourceNotFoundException;
 import ufvjm.reitoria.usuario.infrastructure.repository.UsuarioRepository;
 
 @Service
@@ -13,10 +16,31 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
+    private final PasswordEncoder passwordEncoder;
 
     public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO) {
+        emailExiste(usuarioDTO.getEmail());
+        usuarioDTO.setSenha(passwordEncoder.encode(usuarioDTO.getSenha()));
         Usuario usuario = usuarioConverter.paraUsuario(usuarioDTO);
         return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));
     }
 
+    public void emailExiste(String email) {
+            boolean existe = verificarEmailExistente(email);
+            if (existe) {
+                throw new ConflictException("Email já cadastrado: " + email);
+            }
+    }
+
+    public boolean verificarEmailExistente(String email) {
+        return usuarioRepository.existsByEmail(email);
+    }
+
+    public Usuario buscaUsuarioPorEmail(String email) {
+        return usuarioRepository.findByEmail(email).orElseThrow(() -> new ResourceNotFoundException("Email não encontrado " + email));
+    }
+
+    public void deletaUsuarioPorEmail(String email) {
+        usuarioRepository.deleteByEmail(email);
+    }
 }
--- src/main/java/ufvjm/reitoria/usuario/infrastructure/repository/EnderecoRepository.java
+++ src/main/java/ufvjm/reitoria/usuario/infrastructure/repository/EnderecoRepository.java
@@ -1,8 +1,8 @@
 package ufvjm.reitoria.usuario.infrastructure.repository;
 
-import com.javanauta.aprendendospring.infrastructure.entity.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ufvjm.reitoria.usuario.infrastructure.entity.Endereco;
 
 @Repository
 public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
